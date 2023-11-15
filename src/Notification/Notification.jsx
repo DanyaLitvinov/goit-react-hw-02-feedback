69b337fce3c2b5dc3e366661bddbc2a5a1ebeb64
DanyaLitvinov <vfvdaniel07@gmail.com>
2023-11-15 21:34:56 +0200

    123

--- src/Notification/Notification.jsx
+++ src/Notification/Notification.jsx
@@ -1,22 +1,18 @@
-import style from './Notification.module.css';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
+import styles from './Notification.module.css';
 
-class Notification extends Component {
-  static defaultProps = {
-    message: ' ',
-  };
-  static propTypes = {
-    Message: PropTypes.string,
-  };
-  render() {
-    return (
-      <h2 key={shortid.generate()} className={style.notification}>
-        {this.props.message}
-      </h2>
-    );
-  }
-}
+const Notification = ({ message = ' ' }) => {
+  return (
+    <h2 key={shortid.generate()} className={styles.notification}>
+      {message}
+    </h2>
+  );
+};
+
+Notification.propTypes = {
+  message: PropTypes.string,
+};
 
 export default Notification;
--- src/Statistics/Statistics.jsx
+++ src/Statistics/Statistics.jsx
@@ -1,50 +1,36 @@
-import style from './Statistics.module.css';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
+import styles from './Statistics.module.css';
 
-class Statistics extends Component {
-  static defaultProps = {
-    stat: { good: 0, neutral: 0, bad: 0 },
-    total: 0,
-  };
-  static propTypes = {
-    stat: PropTypes.object.isRequired,
-    total: PropTypes.number,
-  };
+const Statistics = ({ stat = { good: 0, neutral: 0, bad: 0 }, total = 0, positivePercentage }) => {
+  return (
+    <div className={styles.statistics}>
+      <h2 className={styles.statistics__title}>Statistics</h2>
+      <ul className={styles.statistics__list}>
+        {Object.entries(stat).map(([key, value]) => (
+          <li className={styles.statistics__item} key={shortid.generate()}>
+            <span className={styles.statistics__name}>{key}: </span>
+            <span>{value}</span>
+          </li>
+        ))}
+        <li className={styles.statistics__item} key={shortid.generate()}>
+          <span>Total: </span>
+          <span>{total}</span>
+        </li>
+        <li className={styles.statistics__item} key={shortid.generate()}>
+          <span>Positive feedback: </span>
+          <span>{positivePercentage}%</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className={style.statistics}>
-        <h2 className={style.statistics__title}>Statistics</h2>
-        <ul className={style.statistics__list}>
-          {Object.entries(this.props.stat).map(([key, value]) => {
-            return (
-              <li className={style.statistics__item} key={shortid.generate()}>
-                <span
-                  className={style.statistics__name}
-                  key={shortid.generate()}
-                >
-                  {key}:{' '}
-                </span>
-                <span key={shortid.generate()}>{value}</span>
-              </li>
-            );
-          })}
-          <li className={style.statistics__item} key={shortid.generate()}>
-            <span key={shortid.generate()}>Total: </span>
-            <span key={shortid.generate()}>{this.props.total}</span>
-          </li>
-          <li className={style.statistics__item} key={shortid.generate()}>
-            <span key={shortid.generate()}>Positive feedback: </span>
-            <span key={shortid.generate()}>
-              {this.props.positivePercentage}%
-            </span>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+Statistics.propTypes = {
+  stat: PropTypes.object.isRequired,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
+};
 
 export default Statistics;
